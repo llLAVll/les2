cd85cdde2d8a47e3ede5fb2f82c4ffbafbd2af4c
LAV <vipaleksei007@gmal.com>
2024-03-10 21:49:17 +0300

    task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите минимальную температуру в C (Цельсиях) за день");
+int min = 0;
+    min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальную температуру в C (Цельсиях) за день");
+int max = 0;
+    max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Среднесуточная температура: {(max+min)/2}C");
