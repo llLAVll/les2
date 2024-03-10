a13cf6d7ba3911f461b8bb5dc85d3771616fbbce
LAV <vipaleksei007@gmal.com>
2024-03-10 22:12:19 +0300

    Task2

---
+++ Task2.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp3
+{
+    internal class Task2
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Project-2");
+            Console.WriteLine("Введите номер месяц (от 1 до 12)");
+            int num = -1;
+            num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:
+                    Console.WriteLine("Неверно указан месяц!");
+                    break;
+            }
+
+        }
+    }
+}
